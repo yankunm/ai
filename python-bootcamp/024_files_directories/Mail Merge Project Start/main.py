02a5255fb6f9b59ed43e3afdd6dbceece4c40e14
Yankun Meng <yankunmeng@MacBookPro.lan>
2025-10-25 21:20:59 -0400

    readable

--- python-bootcamp/024_files_directories/Mail Merge Project Start/main.py
+++ python-bootcamp/024_files_directories/Mail Merge Project Start/main.py
@@ -1,26 +1,27 @@
 #TODO: Create a letter using starting_letter.txt 
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
+#Save the letters in the folder "ReadyToSend".    
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 # read file and fine where to insert it
-with open("Mail Merge Project Start/Input/Letters/starting_letter.txt") as letter_file:
+with open("Mail Merge Project Start/Input/Letters/starting_letter.txt", mode="r") as letter_file:
     letter_contents = letter_file.read()
 
 # read names file
-with open("Mail Merge Project Start/Input/Names/invited_names.txt") as names_file:
-    names = names_file.readlines() # read lines as a list
+with open("Mail Merge Project Start/Input/Names/invited_names.txt", mode="r") as names_file:
+    # read each line in the string and append them to a list
+    names = names_file.readlines() 
     
-    # remove all the \n from each name
+    # process the names: remove all whitespaces
     for name in names:
-        name = name.strip() # removes any leading or trailing whitespace characters
-        new_letter = letter_contents.replace("[name]", name) # replace placeholder with actual name
-
-        # save the new letter to a file
+        name = name.strip()
+    
+    # create the new letter
+    for name in names:
+        new_letter = letter_contents.replace("[name]", name) 
         with open(f"Mail Merge Project Start/Output/ReadyToSend/letter_for_{name}.txt", mode="w") as completed_letter:
             completed_letter.write(new_letter)
     
